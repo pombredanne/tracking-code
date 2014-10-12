3bde9a376f2a5624b354a79975c4713f1af581ee
Thomas Sileo <thomas.sileo@gmail.com>
2014-10-12 15:10:43 +0200

    add an id to each payload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ Basiclytics.PubSub.sub("/events", function(etype, msg) {
   Basiclytics.debug("got msg " + etype + ":");
   Basiclytics.debug(msg);
   if (Basiclytics._APIendpoint !== "") {
+    msg.id = Basiclytics.Utils.guid();
     Basiclytics.Utils.sendData(etype, msg);
   } 
 });

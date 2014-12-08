0eed1d7c59c24a48f5b5b204a96abe1bede39e6b
Thomas Sileo <thomas.sileo@gmail.com>
2014-12-08 10:29:19 +0100

    Added Basiclytics.Store (localStorage/cookie fallback)

--- modules/store.js
+++ modules/store.js
@@ -1,5 +1,6 @@
 Basiclytics.Store = (function() {
-// see https://github.com/marcuswestin/store.js auto expiration
-  Basiclytics.Utils.Storage.setItem("basiclytics_session_id", JSON.stringify());
-// TODO wrapper du cookie get/set avec le Storage et fallback cookie, et stocker un visitor_id dedans
+  if (Basiclytics.Utils.Storage) {
+    return Basiclytics.Utils.LocalStorage; 
+  }
+  return Basiclytics.Utils.Cookies;
 })();
